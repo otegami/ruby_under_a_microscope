9281a0cc87dd453df695f35d4119f02cc5a9015e
otegami <a.s.takuya1026@gmail.com>
2022-02-25 08:54:04 +0900

    Leart how object is constructed?

--- episode5/code.rb
+++ episode5/code.rb
@@ -10,3 +10,58 @@ puts math = Mathematician.new
 # 16 進数の部分はオブジェクトへの Value ポインタを示す
 puts math.last_name = 'otegami'
 puts math.inspect
+
+puts "-" * 50
+
+puts str = "some string value"
+puts str.instance_variables
+puts str.instance_variable_set("@val", "value one")
+puts str.instance_variables
+puts str.instance_variable_get("@val")
+
+# 全てのオブジェクトは、クラスポインタとインスタンス変数の配列を含む
+
+# require 'benchmark'
+
+# ITERATIONS = 100000
+# GC.disable
+# obj = ITERATIONS.times.map { Class.new.new }
+
+# Benchmark.bm do |bench|
+#   20.times do |count|
+#     bench.report("adding instance variable number #{count + 1}") do
+#       ITERATIONS.times do |n|
+#         obj[n].instance_variable_set("@var#{count}", "value")
+#       end
+#     end
+#   end
+# end
+
+# - RClass
+#   - Method table
+#   - attribute_names
+
+class Mathematician
+  @type = "General"
+
+  def self.type
+    @type
+  end
+
+  def type
+    @type.nil? ? 'nil' : @type
+  end
+end
+
+puts Mathematician.type
+puts Mathematician.new.type
+
+# クラス毎に下記の4つのハッシュテーブルを持つ
+# - 属性名テーブル
+# - メソッドテーブル
+# - クラスインスタンステーブル
+# - ハッシュテーブル
+# - 定数テーブル
+puts ObjectSpace.count_objects[:T_CLASS]
+puts class TestClassCount; end
+puts ObjectSpace.count_objects[:T_CLASS]
